bcb256ef38748c4a859d8cb86e3c14be09b9cbe5
Ike Vidler <izaak.vidler@gmail.com>
2018-07-31 16:34:26 +1000

    delegate calendar

--- Exchange/calendaritemsdev.py
+++ Exchange/calendaritemsdev.py
@@ -12,9 +12,9 @@ config = Configuration(
     server='mail.snellemantom.com.au', credentials=credentials, version=version, auth_type=NTLM
 )
 
-my_account = Account(primary_smtp_address='i.vidler@snellemantom.com.au', credentials=credentials,
+my_account = Account(primary_smtp_address='m.kerrigan@snellemantom.com.au', credentials=credentials,
                      config=config, access_type=DELEGATE)
 
-for item in my_account.inbox.all().order_by('-datetime_received')[:100]:
-    print(item.subject, item.body, item.attachments)
+for item in my_account.calendar.all().order_by('-datetime_received')[:100]:
+    print(item.subject, item.start, item.end, item.location)
 
